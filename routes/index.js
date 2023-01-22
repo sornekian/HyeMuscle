5497fd0df61f6eed38e1aa04475688bcd5ebe7f9
sornekian <sornekian_sarvornek@gmail.com>
2023-01-22 13:39:58 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    prompt: "select_account"
+  }
+));
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/',
+  }
+));
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/');
+  });
+});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
